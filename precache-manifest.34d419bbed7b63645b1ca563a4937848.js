8ebeb2248c8db4f9fd38bd68d5a3da9d38fc0961
ohadomrad570 <ohadomrad570@users.noreply.github.com>
2024-10-21 14:28:57 +0000

    deploy: 5252718dfe2b5b73beba5d8e21f0842c33949a83

---
+++ precache-manifest.34d419bbed7b63645b1ca563a4937848.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82d5964564432df81a70b0f60a92c2f",
+    "url": "https://ohadomrad570.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "95a7ff78809c1a3e8ce0",
+    "url": "https://ohadomrad570.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "f2cc73ed89309c0a1c97",
+    "url": "https://ohadomrad570.github.io/task-management-frontend/static/js/2.e4ae9c54.chunk.js"
+  },
+  {
+    "revision": "95a7ff78809c1a3e8ce0",
+    "url": "https://ohadomrad570.github.io/task-management-frontend/static/js/main.358e9cee.chunk.js"
+  },
+  {
+    "revision": "2d146a60caa03bf9e9d4",
+    "url": "https://ohadomrad570.github.io/task-management-frontend/static/js/runtime~main.4e8bbaa0.js"
+  }
+]);
